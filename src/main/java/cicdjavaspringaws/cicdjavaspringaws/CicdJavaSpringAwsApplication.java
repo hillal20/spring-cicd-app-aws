b9b09e53f1fc8c9c16d711490a6591ef45fed851
Hilal Aissani <hilalaissani@gmail.com>
2020-06-03 17:41:28 -0500

    latest code

---
+++ src/main/java/cicdjavaspringaws/cicdjavaspringaws/CicdJavaSpringAwsApplication.java
@@ -0,0 +1,15 @@
+package cicdjavaspringaws.cicdjavaspringaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJavaSpringAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdJavaSpringAwsApplication.class, args);
+
+		System.out.println(" ====== CICD SPRING AWS IS RUNNING ===== ");
+	}
+
+}
