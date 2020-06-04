ad3679e64c672d9410529deed0440eb7a2a973bf
Hilal Aissani <hilalaissani@gmail.com>
2020-06-04 14:51:29 -0500

    latest

--- src/main/java/cicdjavaspringaws/cicdjavaspringaws/CicdJavaSpringAwsApplication.java
+++ src/main/java/cicdjavaspringaws/cicdjavaspringaws/CicdJavaSpringAwsApplication.java
@@ -2,14 +2,21 @@ package cicdjavaspringaws.cicdjavaspringaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdJavaSpringAwsApplication {
+public class CicdJavaSpringAwsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdJavaSpringAwsApplication.class, args);
 
 		System.out.println(" ====== CICD SPRING AWS IS RUNNING ===== ");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CicdJavaSpringAwsApplication.class);
 	}
 
 }
